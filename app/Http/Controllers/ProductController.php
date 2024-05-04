b83e9742a00871ed33a91c9ade28a4cc460d85b5
muslimkoding <muslimkoding@gmail.com>
2024-05-04 08:26:00 +0700

    menambahkan method edit dan update di controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -75,7 +76,11 @@ class ProductController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        // get product by ID
+        $product = Product::findOrFail($id);
+
+        // render view with product
+        return view('products.edit', compact('product'));
     }
 
     /**
@@ -83,7 +88,49 @@ class ProductController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        // validate form
+        $request->validate([
+            'image' => 'image|mimes:png,jpg,jpeg|max:2048',
+            'title' => 'required|min:5',
+            'description' => 'required|min:10',
+            'price' => 'required|numeric',
+            'stock' => 'required|numeric',
+        ]);
+
+        // get product by ID
+        $product = Product::findOrFail($id);
+
+        // check if image is uploaded
+        if ($request->hasFile('image')) {
+
+            // upload image
+            $image = $request->file('image');
+            $image->storeAs('public/products', $image->hashName());
+            
+            // delete old image
+            Storage::delete('public/products/'. $product->image);
+
+            // update product with new image
+            $product::update([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'description' => $request->description,
+                'price' => $request->price,
+                'stock' => $request->stock,
+            ]);
+
+        } else {
+             // update product without image
+            $product::update([
+                'title' => $request->title,
+                'description' => $request->description,
+                'price' => $request->price,
+                'stock' => $request->stock,
+            ]);
+        }
+
+        // redirect to indix
+        return redirect()->route('products.index')->with(['success' => 'Data berhasil diubah!']);
     }
 
     /**
